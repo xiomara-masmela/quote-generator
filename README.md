80d378f2d696fde422556061d62e6819579a9c1a
xiomara-masmela <xiomara-masmela@gmail.com>
2021-09-01 20:28:21 +1000

    Update js code

--- README.md
+++ README.md
@@ -4,13 +4,6 @@
 
 This was one of the projects I built working towards my JavaScript Algorithms and Data Structures Certification with FreeCodeCamp.
 
-The project was built using, HTML, CSS and jQuery. 
+The project was built using, HTML, CSS and JavaScript. 
 
-As the project was built a couple of years ago, it needs a little bit of love.
-
-### Future Improvements
-
-1. Refactor code 
-2. Use vanilla JavaScript instead of jQuery
-3. Use API to poulate the quotes
-4. Fix twitter funcionality
+The button will generate a new Quote, fetched from https://type.fit/api/quotes api.
--- script.js
+++ script.js
@@ -1,34 +1,30 @@
 "use strict";
 
-let quotes = [
-    "Life isn\'t about finding yourself. Life is about creating yourself.",
-    "The most important thing is to enjoy your life - to be happy - it's all that matters.",
-    "I have found that if you love life, life will love you back.",
+//From: https://dev.to/nehasoni__/random-quote-generator-using-html-css-and-javascript-3gbp
 
-]
+const quoteDiv = document.getElementById('text');
+const authorDiv = document.getElementById('author');
 
-let authors = [
-    "George Bernard Shaw",
-    "Audrey Hepburn",
-    "Arthur Rubinstein"
-]
+const buton = document.getElementById('new-quote')
+const newQuote = async () => {
+    const api = "https://type.fit/api/quotes";
+    const response = await fetch(api);
+    const quotes = await response.json();
+    let randomI = Math.floor(Math.random() * (quotes.length));
 
-let quoteText = " ";
-let quoteAuthor = " ";
+    const quote = quotes[randomI].text;
+    const auth = quotes[randomI].author;
 
 
-function newQuote() {
-    let randomNumber = Math.floor(Math.random() * (quotes.length));
-    quoteText = document.getElementById("text").innerHTML = quotes[randomNumber];
-    quoteAuthor = document.getElementById("author").innerHTML = authors[randomNumber];
+    if(auth == null) {
+        author = "Anonymous";
+    }
 
-   
+    quoteDiv.innerText = quote;
+    authorDiv.innerText = auth;
+
 }
+buton.addEventListener('click', newQuote)
 
-//Tweet Button
-$(document).ready(function () {
-    //your code here
-    $("#tweet-quote").attr("href", "https://twitter.com/intent/tweet");
-  });
-
+ 
 
